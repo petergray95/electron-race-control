ac5f31c2b4bf7de87644cb0af80d85cbe3c573b4
Michael Sansoni <m.h.sansoni@gmail.com>
2019-08-30 09:40:38 +0100

    Improve live session UI

--- app/server/components/SessionLive.js
+++ app/server/components/SessionLive.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { Header, Form } from 'semantic-ui-react';
+import { Header, Form, Segment, Button } from 'semantic-ui-react';
 import { ipcRenderer } from 'electron-better-ipc';
 import Moment from 'react-moment';
 import ipcConstants from '../../../shared/constants/ipc-channels';
@@ -27,6 +27,9 @@ export default class SessionLive extends Component<Props> {
           <Header as="h1" inverted>
             Data Server [LIVE]
           </Header>
+          <Header as="h3" inverted>
+            Configuration
+          </Header>
           <Form.Group widths="equal" inline>
             <Form.Input fluid label="Name" value={session.name} />
             <Form.Input
@@ -40,69 +43,69 @@ export default class SessionLive extends Component<Props> {
           <Form.Input fluid label="Color" value={session.color} />
         </Form>
 
-        <Form>
-          <Form.Group widths="equal">
-            <Form.Button
-              positive
-              labelPosition="left"
-              icon="play"
-              content="Start"
-              onClick={() => {
-                ipcRenderer.send(ipcConstants.COMMAND, {
-                  command: 'server:start',
-                  sessionId: session.sessionId
-                });
-              }}
-            />
-            <Form.Button
-              negative
-              labelPosition="left"
-              icon="stop"
-              content="Stop"
-              onClick={() => {
-                ipcRenderer.send(ipcConstants.COMMAND, {
-                  command: 'server:stop',
-                  sessionId: session.sessionId
-                });
-              }}
-            />
-            <Form.Button
-              color="grey"
-              content="Remove Session"
-              icon="trash"
-              onClick={() => {
-                ipcRenderer.send(ipcConstants.COMMAND, {
-                  command: 'server:remove',
-                  sessionId: session.sessionId
-                });
-              }}
-            />
-            <Form.Button
-              color="blue"
-              content="Export Session"
-              icon="download"
-              onClick={() => {
-                ipcRenderer.send(ipcConstants.COMMAND, {
-                  command: 'server:download',
-                  sessionId: session.sessionId
-                });
-              }}
-            />
-          </Form.Group>
-        </Form>
+        <Segment inverted color="grey">
+          <Button
+            positive
+            labelPosition="left"
+            icon="play"
+            content="Start"
+            onClick={() => {
+              ipcRenderer.send(ipcConstants.COMMAND, {
+                command: 'server:start',
+                sessionId: session.sessionId
+              });
+            }}
+          />
+          <Button
+            negative
+            labelPosition="left"
+            icon="stop"
+            content="Stop"
+            onClick={() => {
+              ipcRenderer.send(ipcConstants.COMMAND, {
+                command: 'server:stop',
+                sessionId: session.sessionId
+              });
+            }}
+          />
+          <Button
+            color="orange"
+            content="Remove Session"
+            icon="trash"
+            onClick={() => {
+              ipcRenderer.send(ipcConstants.COMMAND, {
+                command: 'server:remove',
+                sessionId: session.sessionId
+              });
+            }}
+          />
+          <Button
+            color="blue"
+            content="Export Session"
+            icon="download"
+            onClick={() => {
+              ipcRenderer.send(ipcConstants.COMMAND, {
+                command: 'server:download',
+                sessionId: session.sessionId
+              });
+            }}
+          />
+        </Segment>
 
-        <Header as="h3" inverted>
-          Last record:{' '}
-          {lastRecordValid ? (
-            <Moment unix format="HH:mm:ss.SSS">
-              {lastRecord / 1000}
-            </Moment>
-          ) : null}
-        </Header>
+        <Segment inverted color="grey">
+          <Header as="h3" inverted>
+            Last record:{' '}
+            {lastRecordValid ? (
+              <Moment unix format="HH:mm:ss.SSS">
+                {lastRecord / 1000}
+              </Moment>
+            ) : null}
+          </Header>
 
-        <Header as="h3" inverted>
-          Number of records: {sessionCursorMeta.numberRecords}
-        </Header>
+          <Header as="h3" inverted>
+            Number of records: {sessionCursorMeta.numberRecords}
+          </Header>
+        </Segment>
       </div>
     );
   }

6eee2b33b545816ca43aa30e6246ddc0114e37ee
Michael Sansoni <m.h.sansoni@gmail.com>
2019-09-28 18:18:56 +0100

    Improve strucutre of data to allow multiple different data sources

---
+++ app/server/data/base.js
@@ -0,0 +1,188 @@
+import { v4 as uuid } from 'uuid';
+import fs from 'fs';
+import zlib from 'zlib';
+
+import { updateSession } from '../../../shared/actions/sessions';
+import { addLap } from '../../../shared/actions/laps';
+import { addParticipant } from '../../../shared/actions/participants';
+import { updateCursor } from '../../../shared/actions/cursor';
+import store from '../store';
+
+
+export class BaseDataSession {
+  constructor() {
+    this.data = {};
+    this.buffer = [];
+    this.name = 'Base';
+    this.id = uuid();
+    this.color = '#f44336';
+    this.sessionType = 'base';
+    this.isRunning = false;
+
+    // Session info
+    this.laps = {};
+    this.participants = {};
+    this.details = {};
+  }
+
+  getStoreConfig() {
+    return {
+      sessionId: this.id,
+      name: this.name,
+      sessionType: this.sessionType,
+      color: this.color,
+      isRunning: this.isRunning,
+      details: this.details
+    };
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  setColor(color) {
+    this.color = color;
+  }
+
+  getDetails() {
+    console.log(this);
+    throw new Error('session getDetails not implemented');
+  }
+
+  getParticipants() {
+    console.log(this);
+    throw new Error('session getParticipants not implemented');
+  }
+
+  getLaps() {
+    console.log(this);
+    throw new Error('session getLaps not implemented');
+  }
+
+  exportLap() {
+    console.log(this);
+    throw new Error('session exportLap not implemented');
+  }
+
+  exportLaps(laps) {
+    laps.forEach(lap => {
+      this.exportLap(lap);
+    })
+  }
+
+  addParticipant(participant) {
+    store.dispatch(addParticipant(this.id, participant.id, participant));
+    this.participants[participant.carId] = participant;
+  };
+
+  addLap(carId, lap) {
+    this.participants[carId].laps.push(lap.id);
+
+    store.dispatch(addLap(this.id, lap.id, lap));
+    this.laps[carId].push(lap);
+  };
+
+  addDetails(details) {
+    this.details = details;
+
+    const storeConfig = this.getStoreConfig();
+    store.dispatch(updateSession(this.id, storeConfig));
+  }
+
+  addLaps(laps) {
+    Object.keys(laps).forEach(carId => {
+      const carLaps = laps[carId];
+      carLaps.forEach(lap => {
+        this.addLap(carId, lap);
+      })
+    })
+  };
+
+  addParticipants(participants) {
+    Object.keys(participants).forEach(carId => {
+      const participant = participants[carId];
+      this.addParticipant(participant);
+    });
+  }
+}
+
+export class BaseDataSessionLive extends BaseDataSession {
+  constructor() {
+    super();
+    this.sessionType = 'live';
+    this.client = null;
+  }
+
+  addMessage(message) {
+    const cursor = {
+      sessionId: this.id,
+      values: message,
+      meta: {
+        timestamp: new Date().getTime(),
+        numberRecords: this.data.length,
+        lastRecord: message
+      }
+    };
+    store.dispatch(updateCursor(cursor));
+  }
+
+  start() {
+    console.log(this);
+    throw new Error('session start not implemented');
+  }
+
+  stop() {
+    console.log(this);
+    throw new Error('session stop not implemented');
+  }
+}
+
+export class BaseDataSessionHistoric extends BaseDataSession {
+  constructor() {
+    super();
+    this.name = 'Historic';
+    this.sessionType = 'historic';
+  }
+
+  setData(data) {
+    this.data = data;
+
+    const participants = this.getParticipants();
+    this.addParticipants(participants);
+
+    const laps = this.getLaps();
+    this.addLaps(laps);
+
+    const details = this.getDetails();
+    this.addDetails(details);
+  }
+
+  loadData(filepath) {
+    fs.readFile(filepath, (fsErr, data) => {
+      zlib.inflate(data, (zlibErr, result) => {
+        const parsedData = JSON.parse(result);
+        this.setData(parsedData);
+      });
+    });
+  }
+
+  downloadSession() {
+    const filename = 'export.etx';
+    const blob = {
+      header: {
+        name: this.name
+      },
+      data: { ...this.data }
+    };
+
+    zlib.deflate(JSON.stringify(blob), (deflateErr, buffer) => {
+      if (!deflateErr) {
+        fs.writeFile(filename, buffer, writeErr => {
+          if (writeErr) {
+            console.log(writeErr);
+          }
+        });
+      }
+    });
+  }
+}
---
+++ app/server/data/f12019.js
@@ -0,0 +1,471 @@
+import _ from 'lodash';
+import { v4 as uuid } from 'uuid';
+import fs from 'fs';
+import { flatten } from 'flat';
+import { F1TelemetryClient, constants } from 'f1-telemetry-client';
+import { BaseDataSessionLive, BaseDataSessionHistoric } from './base';
+import {
+  getTimestampGroup,
+  getTimestampGroups,
+  getTimestampGroupRange,
+  getClosestIndexes,
+  getClosestValue,
+  getSimplifiedPoints
+} from './helpers';
+
+
+const { DRIVERS, PACKETS, TEAMS, TRACKS, WEATHER } = constants;
+
+
+export class F12019SessionLive extends BaseDataSessionLive {
+  constructor(opts) {
+    super(opts);
+    this.name = 'Live';
+    this.client = new F1TelemetryClient();
+
+    this.client.on(PACKETS.session, message =>
+      this.parseMessage(PACKETS.session, message)
+    );
+    this.client.on(PACKETS.motion, message =>
+      this.parseMessage(PACKETS.motion, message)
+    );
+    this.client.on(PACKETS.lapData, message =>
+      this.parseMessage(PACKETS.lapData, message)
+    );
+    this.client.on(PACKETS.event, message =>
+      this.parseMessage(PACKETS.event, message)
+    );
+    this.client.on(PACKETS.participants, message =>
+      this.parseMessage(PACKETS.participants, message)
+    );
+    this.client.on(PACKETS.carSetups, message =>
+      this.parseMessage(PACKETS.carSetups, message)
+    );
+    this.client.on(PACKETS.carTelemetry, message =>
+      this.parseMessage(PACKETS.carTelemetry, message)
+    );
+    this.client.on(PACKETS.carStatus, message =>
+      this.parseMessage(PACKETS.carStatus, message)
+    );
+  }
+
+  start() {
+    this.client.start();
+    this.isRunning = true;
+  }
+
+  stop() {
+    this.client.stop();
+    this.isRunning = false;
+  }
+
+  parseMessage(messageType, message) {
+    const start = performance.now();
+
+    const timestamp = new Date().getTime();
+    const timestampRound = getTimestampGroup(timestamp);
+
+    const messageFlat = flatten(message);
+
+    Object.entries(messageFlat).forEach(([key, value]) => {
+      const data = _.get(
+        this.data,
+        [messageType, key, `_${timestampRound}`],
+        []
+      );
+      data.push(value);
+      _.set(this.data, [messageType, key, `_${timestampRound}`], data);
+    });
+
+    const times = _.get(
+      this.data,
+      [messageType, 'times', `_${timestampRound}`],
+      []
+    );
+    times.push(timestamp);
+    _.set(this.data, [messageType, 'times', `_${timestampRound}`], times);
+
+    const duration = performance.now() - start;
+
+    this.addMessage(duration.toFixed(2));
+  }
+}
+
+export class F12019SessionHistoric extends BaseDataSessionHistoric {
+  constructor() {
+    super();
+    this.participants = [...Array(20).keys()].reduce(
+      (o, key) => ({
+        ...o,
+        [key]: []
+      }),
+      {}
+    );
+
+    this.laps = [...Array(20).keys()].reduce(
+      (o, key) => ({
+        ...o,
+        [key]: []
+      }),
+      {}
+    );
+  }
+
+  getDetails() {
+    const data = _.get(this.data, ['data'], {});
+    const timestampGroups = getTimestampGroups(data, 'session');
+    const lastTimestampGroup = timestampGroups[
+      timestampGroups.length - 1
+    ].replace('_', '');
+
+    const sessionTimes = data.session[`_${lastTimestampGroup}`].times;
+
+    return {
+      track:
+        TRACKS[
+          getClosestValue(
+            data,
+            lastTimestampGroup,
+            sessionTimes.length - 1,
+            'session',
+            `m_trackId`
+          )
+        ],
+      weather:
+        WEATHER[
+          getClosestValue(
+            data,
+            lastTimestampGroup,
+            sessionTimes.length - 1,
+            'session',
+            `m_weather`
+          )
+        ]
+    };
+  }
+
+  getDriver(data, timestampGroup, index, participantId) {
+    const driver =
+      DRIVERS[
+        getClosestValue(
+          data,
+          timestampGroup,
+          index,
+          'participants',
+          `m_participants.${participantId}.m_driverId`
+        )
+      ];
+
+    if (driver) {
+      return driver
+    }
+
+    return { abbreviation: '', firstName: '', lastName: '' }
+  }
+
+  getParticipants() {
+    const data = _.get(this.data, ['data'], {});
+    const timestampGroups = getTimestampGroups(data, 'participants');
+    const lastTimestampGroup = timestampGroups[
+      timestampGroups.length - 1
+    ].replace('_', '');
+
+    const participantTimes = data.participants[`_${lastTimestampGroup}`].times;
+
+    const carIds = [...Array(20).keys()];
+    const participants = {};
+
+    carIds.forEach(carId => {
+      const participant = {
+        id: uuid(),
+        carId,
+        sessionId: this.id,
+        laps: [],
+        driver: null,
+        name: null,
+        nationality: null,
+        raceNumber: null,
+        team: null
+      };
+
+      participant.driver = this.getDriver(
+        data,
+        lastTimestampGroup,
+        participantTimes.length - 1,
+        participant.id
+      );
+      participant.name = getClosestValue(
+        data,
+        lastTimestampGroup,
+        participantTimes.length - 1,
+        'participants',
+        `m_participants.${carId}.m_name`
+      );
+      participant.nationality = getClosestValue(
+        data,
+        lastTimestampGroup,
+        participantTimes.length - 1,
+        'participants',
+        `m_participants.${carId}.m_nationality`
+      );
+      participant.raceNumber = getClosestValue(
+        data,
+        lastTimestampGroup,
+        participantTimes.length - 1,
+        'participants',
+        `m_participants.${carId}.m_raceNumber`
+      );
+      participant.team =
+        TEAMS[
+          getClosestValue(
+            data,
+            lastTimestampGroup,
+            participantTimes.length - 1,
+            'participants',
+            `m_participants.${carId}.m_teamId`
+          )
+        ];
+
+      participants[carId] = participant;
+    });
+    return participants
+  }
+
+  getLapInfo(data, carId, indexes, startTime, endTime, lapTime=null) {
+    console.log(this.participants, carId);
+    const participantId = this.participants[carId].id;
+
+    let lap = {
+      id: uuid(),
+      sessionId: this.id,
+      participantId,
+      carId,
+      startTime,
+      endTime,
+      number: getClosestValue(
+        data,
+        indexes.lapData.timestampGroup,
+        indexes.lapData.index,
+        'lapData',
+        `m_lapData.${carId}.m_currentLapNum`
+      ),
+      calculatedLapTime: null,
+      lapTime,
+      isFullLap: null,
+      sector1Time: getClosestValue(
+        data,
+        indexes.lapData.timestampGroup,
+        indexes.lapData.index,
+        'lapData',
+        `m_lapData.${carId}.m_sector1Time`
+      ),
+      sector2Time: getClosestValue(
+        data,
+        indexes.lapData.timestampGroup,
+        indexes.lapData.index,
+        'lapData',
+        `m_lapData.${carId}.m_sector2Time`
+      ),
+      isValid: !getClosestValue(
+        data,
+        indexes.lapData.timestampGroup,
+        indexes.lapData.index,
+        'lapData',
+        `m_lapData.${carId}.m_currentLapInvalid`
+      )
+    };
+
+    lap = {
+      ...lap,
+      sector1Time: lap.sector1Time !== 0 ? lap.sector1Time : null,
+      sector2Time: lap.sector2Time !== 0 ? lap.sector2Time : null
+    };
+
+    lap = {
+      ...lap,
+      sector3Time: lap.lapTime && lap.sector1Time && lap.sector2Time ? (lap.lapTime-lap.sector2Time-lap.sector1Time) : null
+    };
+
+    return lap
+  }
+
+  getLaps() {
+    const carCursors = [...Array(20).keys()].reduce(
+      (o, key) => ({
+        ...o,
+        [key]: { cursor: { lapNumber: null, startTime: null } }
+      }),
+      {}
+    );
+
+    const laps = [...Array(20).keys()].reduce(
+      (o, key) => ({
+        ...o,
+        [key]: []
+      }),
+      {}
+    );
+
+    const data = _.get(this.data, ['data'], {});
+    const lapData = _.get(data, ['lapData'], {});
+
+    Object.keys(lapData).forEach((timestampGroup, timestampGroupIndex) => {
+      const timestamps = lapData[timestampGroup].times;
+      timestamps.forEach((timestamp, timestampIndex) => {
+        const currentIndexes = getClosestIndexes(data, timestamp);
+
+        Object.keys(carCursors).forEach(carId => {
+          const carCursor = carCursors[carId];
+
+          const lapNumber = getClosestValue(
+            data,
+            currentIndexes.lapData.timestampGroup,
+            currentIndexes.lapData.index,
+            'lapData',
+            `m_lapData.${carId}.m_currentLapNum`
+          );
+
+          if (!carCursor.startTime) {
+            carCursor.startTime = timestamp;
+          }
+
+          if (!carCursor.lapNumber) {
+            carCursor.lapNumber = lapNumber;
+          }
+
+          if (carCursor.lapNumber !== lapNumber) {
+            // We have just completed a lap
+            const lapCompleteTimestamp = timestamps[timestampIndex - 1];
+            const previousIndexes = getClosestIndexes(
+              data,
+              lapCompleteTimestamp
+            );
+
+            let lap = this.getLapInfo(
+              data,
+              carId,
+              previousIndexes,
+              carCursor.startTime,
+              lapCompleteTimestamp,
+              getClosestValue(
+                data,
+                currentIndexes.lapData.timestampGroup,
+                currentIndexes.lapData.index,
+                'lapData',
+                `m_lapData.${carId}.m_lastLapTime`
+              )
+            );
+
+            lap = {
+              ...lap,
+              isFullLap: true,
+              calculatedLapTime:
+                (lapCompleteTimestamp - carCursor.startTime) / 1000
+            };
+
+            laps[carId].push(lap);
+
+            carCursor.lapNumber = lapNumber;
+            carCursor.startTime = timestamp;
+          }
+
+          if (
+            timestampGroupIndex + 1 === Object.keys(lapData).length &&
+            timestampIndex + 1 === timestamps.length &&
+            lapNumber !== 0
+          ) {
+            let lap = this.getLapInfo(
+              data,
+              carId,
+              currentIndexes,
+              carCursor.startTime,
+              timestamp
+            );
+
+            lap = {
+              ...lap,
+              isFullLap: false,
+              calculatedLapTime: timestamp - carCursor.startTime
+            };
+
+            laps[carId].push(lap);
+          }
+        });
+      });
+    });
+    return laps;
+  };
+
+  exportLap(carId: string, lapId: string, simplify: boolean = true) {
+    const lap = this.laps[carId].filter(item => item.id === lapId)[0];
+
+    const channels = [
+      ['carTelemetry', 'm_carTelemetryData.0.m_speed'],
+      ['carTelemetry', 'm_carTelemetryData.0.m_throttle'],
+      ['carTelemetry', 'm_carTelemetryData.0.m_brake']
+    ];
+
+    const timeGroups = getTimestampGroupRange(lap.startTime, lap.endTime);
+
+    const output = {
+      session: this.getStoreConfig(),
+      lap,
+      participant: this.participants[lap.carId],
+      data: {}
+    };
+
+    channels.forEach(([group, channel]) => {
+      const key = `${group}.${channel}`;
+      const dataArray = [];
+
+      timeGroups.forEach((timeGroup, groupIndex) => {
+        const groupData = _.get(
+          this.data,
+          ['data', group, `_${timeGroup}`],
+          {}
+        );
+
+        let subTimes = [];
+        let subValues = [];
+
+        switch (groupIndex) {
+          case 0: {
+            const firstIndex = groupData.times.findIndex(
+              time => time >= lap.startTime
+            );
+            subTimes = groupData.times.slice(firstIndex);
+            subValues = groupData[channel].slice(firstIndex);
+            break;
+          }
+          case groupData.length - 1: {
+            const lastIndex = groupData.times.findIndex(
+              time => time >= lap.endTime
+            );
+            subTimes = groupData.times.slice(0, lastIndex);
+            subValues = groupData[channel].slice(0, lastIndex);
+            break;
+          }
+          default: {
+            subTimes = groupData.times;
+            subValues = groupData[channel];
+            break;
+          }
+        }
+        subTimes.forEach((time, index) => {
+          dataArray.push({ time, value: subValues[index] });
+        });
+      });
+
+      output.data[key] = simplify
+        ? getSimplifiedPoints(dataArray)
+        : dataArray;
+    });
+
+    fs.writeFile(
+      `./EXPORT_DATA/${this.name}_${lap.number}.json`,
+      JSON.stringify(output),
+      'utf8',
+      () => {}
+    );
+  }
+}
